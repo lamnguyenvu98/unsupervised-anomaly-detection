99d83bc8832217e67c8a67aa6ed25cd48817418f
lamnguyenvu98 <lamnguyenvu98@gmail.com>
2023-02-24 20:32:30 +0700

    updated

--- DFR/inference_image.py
+++ DFR/inference_image.py
@@ -30,21 +30,23 @@ tfms = transforms.Normalize(
 frame = cv2.imread('/home/pep/Drive/PCLOUD/Projects/Anomaly-Detection/DFR/003.png')
 h, w = frame.shape[:2]
 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-frame = cv2.resize(frame, (256, 256))
+frame = cv2.resize(frame, (224, 224))
 data = torch.tensor(frame/255.).permute(2, 0, 1).unsqueeze(0).float()
 data = tfms(data)
 scores = model.predict(data)
 
+print('threshold', model.threshold)
+print('score', scores)
+
 # 0: normal, 1: anomaly
-prediction = torch.any((scores > model.threshold).view(-1), dim=-1).int().item()
+prediction = np.any(scores > model.threshold).astype('int')
 print('Prediction: ', classes[prediction])
 
-mask_ng = scores.detach().cpu().numpy().squeeze()
-# mask_ng = cv2.resize(mask_ng, (256, 256))
-mask_ng = np.where(mask_ng > model.threshold, 1, 0)
-colormap = cv2.applyColorMap((mask_ng * 255).astype(np.uint8), cv2.COLORMAP_INFERNO)
+mask = np.where(scores.squeeze(0) > model.threshold, 1, 0)
+# mask = cv2.resize((mask[..., None] * 255).astype(np.uint8), (w, h))
+colormap = cv2.applyColorMap((mask[..., None] * 255).astype(np.uint8), cv2.COLORMAP_INFERNO)
 anomaly_frame = cv2.addWeighted(colormap, 0.7, frame, 0.7, 0)
 plt.imshow(anomaly_frame)
 plt.show()
-plt.imshow(mask_ng, cmap='gray')
+plt.imshow(mask, cmap='gray')
 plt.show()
